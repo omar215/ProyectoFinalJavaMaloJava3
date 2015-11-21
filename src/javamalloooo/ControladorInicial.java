14832801c527304652886c8351baf8a96f344c97
como-quesito <rapidclimate@outlook.com>
2015-11-21 01:46:03 -0600

    maloooooo

---
+++ src/javamalloooo/ControladorInicial.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javamalloooo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ *
+ * @author Campitos Ley <campitos-ley.com>
+ */
+@Controller
+@RequestMapping("/")
+public class ControladorInicial {
+    
+    @RequestMapping("/")
+    @CrossOrigin
+    @ResponseBody String hola(){
+        return "Hola desde el spring boot";
+    }
+    
+}
